7194fcd9fe80fdf49dac0e7841160ef6bcf4b5fc
Axytric <icyplayz1022@gmail.com>
2024-05-24 22:10:07 +0800

    Delete database.php

--- database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Create connection
-$conn = new mysqli($database_host, $database_username, $database_password, $database_name);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
